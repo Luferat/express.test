bfebfedb240e03918ce0e17b7b929bb3f7ea3efd
Luferat <contato@luferat.net>
2023-02-12 13:07:28 -0300

    Criando o aplicativo

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('<h1>Olá Mundo!</h1>');
-});
-
-app.listen(port, () => {
-  console.log(`Executando servidor em http://localhost:${port}`);
-});
+// index.js
